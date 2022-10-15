76ffbd182a2c410398f32cb933d8f4966c0d6091
Andre Alves <andrealvesaraujo.1995@gmail.com>
2022-10-15 16:40:54 -0300

    149. Mudando a quantidade

--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import {MdDelete} from 'react-icons/md'
+import {MdDelete, MdAddCircle, MdRemoveCircle} from 'react-icons/md'
 
 import {useSelector, useDispatch} from 'react-redux'
-import { removeReserve } from '../../store/modules/reserve/actions'
+import { removeReserve, updateAmountReserve } from '../../store/modules/reserve/actions'
 
 import './style.css'
 
@@ -16,6 +16,14 @@ export default function Reservas() {
     dispatch(removeReserve(id))
   }
 
+  function decrementAmount(trip){
+    dispatch(updateAmountReserve(trip.id, trip.amount - 1))
+  }
+  
+  function incrementAmount(trip){
+    dispatch(updateAmountReserve(trip.id, trip.amount + 1))
+  }
+
   return (
     <div>
         <h1 className='title'>Você solicitou {reserves.length} reservas</h1>
@@ -24,7 +32,23 @@ export default function Reservas() {
           <div className='reservas' key={reserve.id}>
             <img src={reserve.image} alt={reserve.title}/>
             <strong>{reserve.title}</strong>
-            <span>Quantidade: {reserve.amount}</span>
+            
+            <div id='amount'>
+              <button
+                type='button'
+                onClick={()=>decrementAmount(reserve)}
+              >
+                <MdRemoveCircle size={25} color="#191919" />
+              </button> 
+              <input type="text" readOnly value={reserve.amount}/>
+              <button
+                type='button'
+                onClick={()=>incrementAmount(reserve)}
+              >
+                <MdAddCircle size={25} color="#191919" />
+              </button> 
+            </div>
+            
             <button
               type='button'
               onClick={()=>handleRemove(reserve.id)}
